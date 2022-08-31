a82a41046877d8387cc27f5b6a37883fb11935b6
santiago1023 <santiago.bedoyad@udea.edu.co>
2022-08-31 12:50:30 -0500

    clase 3 (19 agosto 2022)

---
+++ app.py
@@ -0,0 +1,18 @@
+from distutils.command.config import config
+from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/')
+def hello_world():
+    dictToReturn = {
+        'message':'Hola mundo'
+    }
+    return jsonify(dictToReturn), 201
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=config["PORT"], debug=True)
